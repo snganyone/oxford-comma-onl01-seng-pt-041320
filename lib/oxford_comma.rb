ea900bc9e6c667c60823d228922f54860a9de456
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-22 03:22:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,11 @@ def oxford_comma(array)
     return array.join(", ")
   elsif array.length == 2
     return array.join(" and ")
+<<<<<<< HEAD
   elsif array.length >= 3
+=======
+  elsif array.length => 3
+>>>>>>> 26d5c9a2f4e32d4106d12f503b34c5f40a0e9816
     return array[0..-2].join(", ") + ", and " + array[-1]
   end
 end
